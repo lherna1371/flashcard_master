0a6927683a5f4535e2d7e8e2aefa3c8941e13a27
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-01 13:15:04 -0500

    Round and Score work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,25 +13,55 @@ get '/user/:id' do
   erb :user
 end
 
-get '/game' do
-  card = Deck.first.cards.first
-
-  erb :game, layout: false, locals: {card: card}
-end
-
 get '/scores_display' do
 
 end
 
-get '/scores' do
+get '/scores/:id' do
+ 
+  round = []
+  Round.all.each do |rv|
+   round << Round.where(user_id: current_user.id)
+  end
+  @user = current_user.email
+  @round = round
+  decks = []
+  @round.each do |n|
+     decks << Deck.where(id: n.first.deck_id)
+  end
+  @decks = decks
   erb :scores
 end
 
+
+
 get '/logout' do
   session.clear
-  erb :index
+  redirect('/')
 end
-  
+
+
+
+# get '/game' do
+#   decks = []
+#   cards = []
+#   ids_decks = []
+#   params[:id_deck].each do |x,y|
+#     if y == "on"
+#       decks << Deck.find(x)
+#       ids_decks << x
+#     end
+#   end
+#   ids_decks.each do |v|
+#     Card.where(deck_id: v).each do |c|
+#       cards << c
+#     end
+#   end
+#   @cards = cards
+#   erb :game
+# end
+
+
 # POST ===========================================
 
 post '/register' do
@@ -52,31 +82,71 @@ post '/user' do
   end
 end
 
-post '/game' do
 
+# post '/game' do
+#   decks = []
+#   cards = []
+#   ids_decks = []
+#   params[:id_deck].each do |x,y|
+#     if y == "on"
+#       decks << Deck.find(x)
+#       ids_decks << x
+#     end
+#   end
+#   ids_decks.each do |v|
+#     Card.where(deck_id: v).each do |c|
+#       cards << c
+#     end
+#   end
+#   redirect('/game')
+# end
+
+post '/game' do
+  cards = []
+  # params[:id_deck].each do |x,y|
+  #   if y == "on"
+  #     Card.where(deck_id: x).each do |c|
+  #       cards << c
+  #     end  
+  #   end
+  # end
+  @p = params[:options]
+  # card_coll = Card.where(deck_id: params[:options])
+  Card.all.each do |x|
+    if x.deck_id.to_i == params[:options].to_i
+      cards << x
+    end
+  end
+  @cards = cards
+  p @current_val = @cards.first.id
+  # @cards = cards
+  # @current_val = @cards.first.id
+  erb :game
 end
 
 
-post '/responses_answers' do
-  current_card = Card.find((params[:game][:id]).to_i)
-  user_answer = params[:game][:answer].downcase
-  @cci = current_card.id
-  @cca = current_card.answer
-  @ccq = current_card.question
-  @ccd = current_card.deck_id
-  @ua = user_answer
+post '/game/:id' do 
+  cards = [] 
+  current_card = Card.find(params[:id])
+  Card.all.each do |x|
+    if x.deck_id.to_i == current_card.deck_id.to_i
+      cards << x
+    end
+  end
+  @cards = cards
+  @score = Guess.where(correctness: true).count
+  if current_card.id == Card.where(deck_id: current_card.deck_id).last.id
+    round = Round.create(user_id: current_user.id, deck_id: current_card.deck_id, score: @score)
+    redirect('/scores')
+  end
+    user_answer = params[:game][:answer].downcase
   if current_card.answer.downcase == user_answer
     guess = Guess.create(response: user_answer, correctness: true, card_id: current_card.id, round_id: 1)
-    @p = "YOU WERE RIGHT"
-    correct_guesses = Guess.where(correctness: true).count
-    @c = Guess.last 
-    @g = Guess.all
-    @d = Guess.last.response
-    @score = correct_guesses
-    erb :game, layout: false
   else
-    @p_wrong = "this is working"
-  end
+    @p = "SUPER WRONG"
+  end 
+  @current_val = current_card.id.to_i + 1
+  erb :game
 end
 
 
